6152078608d9bfab20c9e1807c8d16582a6bab56
synodriver <diguohuangjiajinweijun@gmail.com>
2024-10-26 21:12:03 +0800

    Update setup.py
    add 3.13 support

--- setup.py
+++ setup.py
@@ -143,6 +143,7 @@ def main():
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
             "Programming Language :: Python :: 3.12",
+            "Programming Language :: Python :: 3.13",
             "Programming Language :: Python :: Implementation :: CPython",
             "Programming Language :: Python :: Implementation :: PyPy",
         ],
